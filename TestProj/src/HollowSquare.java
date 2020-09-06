1f7e2a4ad72a33c03991fde829e3b15f3fbecad2
XerckM <mercadoxerckiem@gmail.com>
2020-09-05 23:52:40 -0400

    added code comments

--- TestProj/src/HollowSquare.java
+++ TestProj/src/HollowSquare.java
@@ -1,45 +1,52 @@
 import java.util.*;
 
 public class HollowSquare {
-    private static Scanner in = new Scanner(System.in);
+    private static Scanner in = new Scanner(System.in); // created class level scanner variable "in" to apply through the whole class instead of creating more scanner variables
     public static void main(String[] args){
         char ans;
-        boolean init = true;
+        boolean init = true; // created boolean to loop through first user input
         Scanner in = new Scanner(System.in);
         System.out.print("Do you want to create a hollow box (Y/N)? ");
         ans = in.next().charAt(0);
         do{
-            if ((ans == 'Y') || (ans == 'y')){
+            if ((ans == 'Y') || (ans == 'y')){ // the rest of the code executes when user inputs Y or y
                 int range = getRange();
                 printBox(range);
                 System.out.println();
             }
-            else if ((ans == 'N') || (ans == 'n')){
+            else if ((ans == 'N') || (ans == 'n')){ // breaks the loop and exits the program when user input is N or n
                 in.close();
                 break;
             }
             else{
-                System.out.println("Only answer with 'Y' or 'y' for Yes, and 'N' or 'n' for No.");
+                System.out.println("Only answer with 'Y' or 'y' for Yes, and 'N' or 'n' for No."); // raises error message when user input is other than intended
             }
             System.out.print("Do you want to create a hollow box (Y/N)? ");
             ans = in.next().charAt(0);
         } while (init);
     }
     public static int getRange(){
+        /*
+
+        This method will take user input and will check for validity.
+        Try-catch is used to check user input and will raise an error if the-
+        -character is not an integer
+
+        */
         int range = 0;
         boolean valid = false;
         System.out.print("How many chars/last row? ");
-        do{
+        do{ // do-while loop to catch error InputMismatchException for non-integer characters
             try{
                 range = in.nextInt();
-                if (isValidRange(range) == true){
+                if (isValidRange(range) == true){ // uses the isValidRange() method to validate if user input is within range and exits the loop and returns the range
                     valid = true;
                     return range;
                 }
                 else{
-                    System.out.print("ERROR! Valid range 5 - 21. How many chars/last row? ");
+                    System.out.print("ERROR! Valid range 5 - 21. How many chars/last row? "); // raises error message when input is not in the valid range
                 }
-            } catch (InputMismatchException e){
+            } catch (InputMismatchException e){ // when non-integer character is passed it raises this error
                 System.out.print("Not an integer! Try again! How many chars/last row? ");
                 in.next();
             }
@@ -47,12 +54,22 @@ public class HollowSquare {
         return range;
     }
     public static boolean isValidRange(int range){
+        /*
+
+        This method returns a boolean if the user input is within valid range
+
+        */
         if ((range >= 5) && (range <= 21)){
             return true;
         }
         return false;
     }
     public static void printBox(int range){
+        /*
+
+        This method will print the characters as a box if the user input range is accepted.
+
+        */
         System.out.print("What character? ");
         char value = in.next().charAt(0);
         System.out.println();
